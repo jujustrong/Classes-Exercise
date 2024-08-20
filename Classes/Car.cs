c6d34105d101e4bf30a4ca0757fe56e8074335ea
Julian Armstrong <julian.armstrong8@gmail.com>
2024-08-20 13:16:52 -0600

    completed ex1

---
+++ Classes/Car.cs
@@ -0,0 +1,11 @@
+using System.Xml.Schema;
+
+namespace Classes;
+
+public class Car
+{
+    public Model()
+    {
+        
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,21 @@
 ﻿namespace Classes
 {
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+    }
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car ferarri = new Car();
+            Console.WriteLine(ferarri.Make = "Ferrari");
+            Console.WriteLine(ferarri.Model = "488 GTB Base");
+            Console.WriteLine(ferarri.Year = "2018");
+            
+            
         }
     }
 }
